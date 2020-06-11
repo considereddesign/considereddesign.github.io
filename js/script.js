a6c81daf5a2596f42e59f3235ef4eebb54a02d56
jaibles <ciaran@visible.vc>
2020-06-11 20:57:22 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function()    {
+mediumZoom("#zoom-default");
+mediumZoom("#zoom-margin", {
+  margin: 48
+});
+mediumZoom("#zoom-background", {
+  background: "#131711"
+});
+mediumZoom("#zoom-scrollOffset", {
+  scrollOffset: 0
+});
+mediumZoom("#zoom-metaClick", {
+  metaClick: false
+});
+mediumZoom("[data-zoom]");
+
+var zoomToTrigger = mediumZoom("#zoom-trigger");
+var button = document.querySelector("#btn-trigger");
+button.addEventListener("click", function() {
+  zoomToTrigger.show();
+});
+
+var zoomToDetach = mediumZoom("#zoom-detach");
+zoomToDetach.addEventListeners("hidden", zoomToDetach.detach);
+});
